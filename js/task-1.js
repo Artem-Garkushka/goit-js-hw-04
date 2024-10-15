5e6aea8f591d1160f0e7368d5441c79b0e6d020b
Artem Garkushka <Artem.Garkushka@gmail.com>
2024-10-15 17:53:46 +0300

    js

--- js/task-1.js
+++ js/task-1.js
@@ -1,17 +1,11 @@
-function makeTransaction(quantity, pricePerDroid){
-    const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function isEnoughCapacity(products, containerSize){
+    let totalQuantity = 0;
+    for(const quantity of Object.values(products)){
+        totalQuantity += quantity;
+    }
+    return totalQuantity <= containerSize;
 }
-console.log(makeTransaction(5, 3000));
-
-function makeTransaction(quantity, pricePerDroid){
-    const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
-console.log(makeTransaction(3, 1000));
-
-function makeTransaction(quantity, pricePerDroid){
-    const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
-console.log(makeTransaction(10, 500));
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
